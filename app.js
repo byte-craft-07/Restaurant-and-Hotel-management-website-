63a4f590bddf49061c3753f564541704c205d2d7
Ajay <ajaykterha@gmail.com>
2026-06-13 13:10:57 +0530

    Remove root dotenv dependency from start entry

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const path = require("path");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: path.join(__dirname, "server", ".env") });
-dotenv.config({ path: path.join(__dirname, ".env") });
-
 require("./server/src/server");
